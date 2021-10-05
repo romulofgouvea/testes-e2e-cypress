e865924986920068712e406363236076e135f27b
romulofgouvea <romulofgouvea@gmail.com>
2021-10-05 09:02:28 -0300

    Elementos basicos

--- cypress/integration/5basic.spec.js
+++ cypress/integration/5basic.spec.js
@@ -1,6 +1,28 @@
 describe('Deve visitar uma pagina e fazer uma asertiva do titulo', () => {
     it('Deve visitar a pagina', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        //assert
         cy.title().should('be.equal', 'Campo de Treinamento')
+        cy.title().should('contain','Campo')
+
+        
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .should('contain','Campo')
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain','Campo')
+    })
+
+    //Aula 8
+    it.only('Encontrar e interagir com o botão', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!')
+        
     })
 });
